823e91c26536235332ef71f8b09eccbc7ccd305b
nvanzm <noach.ambachtsheer@gmail.com>
2025-12-11 10:02:40 +0100

    Logo iin navbar
    - ons logo in de navbar gezet volgens het ontwerp (later nog kijken naar de kleur van de navbar zelf omdat die clasht met het logo)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import { NavLink, useLocation } from 'react-router-dom';
 
+import '../styles/Navbar.css';
+
 function Navbar() {
     const location = useLocation();
     const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
@@ -7,7 +9,10 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="nav-container">
-                <p className="logo">Flora Veiling</p>
+                <p className="logo">
+                    <img className="logo-img" src="/logo-flora-veiling.png" />
+                </p>
+                
                 <ul className="nav-menu">
                     {isAuthPage ? (
                         <>
