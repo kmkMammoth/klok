3e0937af37ed88e30383550811aa6297b995f413
nvanzm <noach.ambachtsheer@gmail.com>
2026-01-08 21:34:56 +0100

    Model aangemaakt en gevuld
    Vanaf hier kan Mike verder met de Model/Migrations

---
+++ backend/veilingklok/Models/GekochtProduct.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace veilingklok.Models;
+
+public class GekochtProduct
+{
+    [Key]
+    [Column("koper_id")]
+    public int? gebruiker_id { get; set; }
+
+    [Required, ForeignKey("Product")]
+    [Column("product_id")]
+    public int? ProductId { get; set; }
+
+    [Column("hoeveelheid")]
+    public int? Hoeveelheid { get; set; }
+
+    [Column("koopprijs", TypeName = "decimal(10,2)")]
+    public decimal? KoopPrijs { get; set; }
+}
